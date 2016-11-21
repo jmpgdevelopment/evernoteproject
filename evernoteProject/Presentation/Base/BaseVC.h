6d291def35877e69a1f885fc4ec40ed22a3c1036
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-21 23:39:22 +0100

    [FEAT] Added BaseVC to manage UI Messager from all ViewControllers

--- evernoteProject/Presentation/Base/BaseVC.h
+++ evernoteProject/Presentation/Base/BaseVC.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BaseVC : UIViewController
+@interface BaseVC : UIViewController <UINavigationControllerDelegate>
+
+- (void)showLoading;
+- (void)dismissLoading;
+- (void)showMessage:(NSString *)message;
+- (void)showSuccess;
+- (void)showError:(NSError *)error;
 
 @end
