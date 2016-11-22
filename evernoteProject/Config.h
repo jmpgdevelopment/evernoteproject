0871efd3b83918802d035a7e89583a2f9c249da3
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-22 22:05:02 +0100

    [FEAT][CONFIG] Added new values to Config.h & Info.plist

--- evernoteProject/Config.h
+++ evernoteProject/Config.h
@@ -12,4 +12,11 @@
 
 #endif /* Config_h */
 
+static NSString* const k_EvernoteAPIString = @"https://sandbox.evernote.com/";
+static NSString* const k_DeveloperToken = @"S=s1:U=931c9:E=15fe0705460:C=15888bf24d0:P=1cd:A=en-devtoken:V=2:H=62aaa453d66578e0fe397b001ac3cfe7";
+static NSString* const k_NoteStoreURL = @"https://sandbox.evernote.com/shard/s1/notestore";
+
+static NSString* const k_ConsumerKey = @"jmparedesg";
+static NSString* const k_ConsumerSecret = @"c029ea983fa2d16f";
+
 
