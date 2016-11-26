679325fe6b9569fd53f9f6b828595f31bf06ea97
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-26 15:19:07 +0100

    [FEAT][NOTES] Added NotesTVCell to show note data

---
+++ evernoteProject/Presentation/Notes/NoteTVCell.h
@@ -0,0 +1,13 @@
+//
+//  NoteTVCell.h
+//  evernoteProject
+//
+//  Created by Jose Manuel Paredes Garcia on 25/11/16.
+//  Copyright © 2016 Jose M Paredes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteTVCell : UITableViewCell
+
+@end
---
+++ evernoteProject/Presentation/Notes/NoteTVCell.m
@@ -0,0 +1,24 @@
+//
+//  NoteTVCell.m
+//  evernoteProject
+//
+//  Created by Jose Manuel Paredes Garcia on 25/11/16.
+//  Copyright © 2016 Jose M Paredes. All rights reserved.
+//
+
+#import "NoteTVCell.h"
+
+@implementation NoteTVCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
