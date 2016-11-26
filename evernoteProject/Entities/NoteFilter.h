fe748cbf20c8c22dc74a248abd47114bfbbd5b87
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-26 19:59:29 +0100

    [FEAT][NOTES] Added NoteFilter Entity

---
+++ evernoteProject/Entities/NoteFilter.h
@@ -0,0 +1,16 @@
+//
+//  NoteFilter.h
+//  evernoteProject
+//
+//  Created by Jose Manuel Paredes Garcia on 26/11/16.
+//  Copyright © 2016 Jose M Paredes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NoteFilter : NSObject
+
+@property (nonatomic, strong) NSString *optionLabel;
+@property (nonatomic) BOOL filterActivedSwitch;
+
+@end
---
+++ evernoteProject/Entities/NoteFilter.m
@@ -0,0 +1,13 @@
+//
+//  NoteFilter.m
+//  evernoteProject
+//
+//  Created by Jose Manuel Paredes Garcia on 26/11/16.
+//  Copyright © 2016 Jose M Paredes. All rights reserved.
+//
+
+#import "NoteFilter.h"
+
+@implementation NoteFilter
+
+@end
