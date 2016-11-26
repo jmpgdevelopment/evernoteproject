c1ba2482d19ff8d61f33118255888d7ddb54bbf7
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-26 15:21:22 +0100

    [FEAT][CONFIG] Changed developerToken & consumerKey & consumerSecret

--- evernoteProject/Config.h
+++ evernoteProject/Config.h
@@ -13,10 +13,10 @@
 #endif /* Config_h */
 
 static NSString* const k_EvernoteAPIString = @"https://sandbox.evernote.com/";
-static NSString* const k_DeveloperToken = @"S=s1:U=931c9:E=15fe0705460:C=15888bf24d0:P=1cd:A=en-devtoken:V=2:H=62aaa453d66578e0fe397b001ac3cfe7";
+static NSString* const k_DeveloperToken = @"S=s1:U=931c9:E=15ff506c4c0:C=1589d559730:P=1cd:A=en-devtoken:V=2:H=77d86a42095c2de69bb24f10b91bc3a5";
 static NSString* const k_NoteStoreURL = @"https://sandbox.evernote.com/shard/s1/notestore";
 
-static NSString* const k_ConsumerKey = @"jmparedesg";
-static NSString* const k_ConsumerSecret = @"c029ea983fa2d16f";
+static NSString* const k_ConsumerKey = @"jmpg";
+static NSString* const k_ConsumerSecret = @"95ab0a74ef9b5e6e";
 
 
