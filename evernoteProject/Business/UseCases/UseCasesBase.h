bf213e0e0031c3f9b773e464ff5d728447bd3643
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-26 16:30:28 +0100

    [FEAT] Added UseCasesBase & UseCaseBaseInterface

--- evernoteProject/Business/UseCases/UseCasesBase.h
+++ evernoteProject/Business/UseCases/UseCasesBase.h
@@ -7,7 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "UseCasesBaseInterface.h"
 
-@interface UseCasesBase : NSObject
+@interface UseCasesBase : NSObject <UseCasesBaseInterface>
 
 @end
---
+++ evernoteProject/Business/UseCases/UseCasesBaseInterface.h
@@ -0,0 +1,13 @@
+//
+//  UseCasesBaseInterface.h
+//  evernoteProject
+//
+//  Created by Jose Manuel Paredes Garcia on 26/11/16.
+//  Copyright © 2016 Jose M Paredes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol UseCasesBaseInterface <NSObject>
+
+@end
