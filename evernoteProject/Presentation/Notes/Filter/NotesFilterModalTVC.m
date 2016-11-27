ef222ab22191654b7fc1adf976dd14d43b9f0087
Jose Manuel Paredes Garcia <josemanuelparedesgarcia@gmail.com>
2016-11-27 12:39:59 +0100

    [FIX][NOTES] Changed labels for strings resources

--- evernoteProject/Presentation/Notes/Filter/NotesFilterModalTVC.m
+++ evernoteProject/Presentation/Notes/Filter/NotesFilterModalTVC.m
@@ -32,16 +32,18 @@
 - (void)configView	{
 
     self.navigationItem.title = NSLocalizedString(@"notesFilter", @"notesFilter");
-    [self showLoading];
     [self getFilterOptions];
 }
 
 - (void)getFilterOptions    {
 
-    self.filterLabelsArray = @[@"Order by title", @"Order By Creation", @"Order By Modified"];
+    self.filterLabelsArray = @[NSLocalizedString(@"orderByTitle", @"orderByTitle"),
+                               NSLocalizedString(@"orderByCreationDate", @"orderByCreationDate"),
+                               NSLocalizedString(@"orderByModificationDate", @"orderByModificationDate")];
     [self.tableView reloadData];
-    [self dismissLoading];
 }
+
+#pragma mark - UI Actions
 
 - (IBAction)okAction:(id)sender {
 
@@ -94,50 +96,5 @@
             break;
     }
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
